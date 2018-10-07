6cf619c0ec5527d8e693ecc81694f5a303e2538f
kszlak <klonczyk@gmail.com>
2018-10-07 22:22:30 +0200

    Add definition 4.6

--- 3.1_file.py
+++ 3.1_file.py
@@ -1,18 +1,20 @@
-#3.1 Promptthe user for hours and rate per hour using input.
+#4.6 Add definition
+# Promptthe user for hours and rate per hour using input.
 
+def computepay(h,r):
+    hrs = input("Enter Hours:")
+    h = float(hrs)
+    rate = input("Enter Rate:")
+    r = float(rate)
+    if h<= 40:
+        i = h*r
+        return i
+    else:
+        x=h-40
+        s=h-x
+        i2 =s*r+x*r*1.5
+        return i2
 
+p = computepay(10,20)
 
-hrs = input("Enter Hours:")
-
-h = float(hrs)
-
-rate = input("Enter Rate:")
-
-r = float(rate)
-
-if h<= 40:
-    print("Pay ",h*r)
-else:
-    x=h-40
-    s=h-x
-    print(s*r+x*r*1.5)
+print("Pay",p)
