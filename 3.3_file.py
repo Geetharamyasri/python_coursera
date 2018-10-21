4e4e96988e9eb28731e7f1b314ab3008ccd25d42
kszlak <kszlak@users.noreply.github.com>
2018-10-21 21:36:54 +0200

    Add function

--- 3.3_file.py
+++ 3.3_file.py
@@ -1,19 +1,24 @@
-score = input("Enter score:")
-score=float(score)
+def computegrade():
 
-if score > 1.0:
-    print("Error: Out of range")
-    quit()
-if score <0:
-    print("Error: Out of range")
-    quit()
-elif score>=0.9:
-    print("A")
-elif score>=0.8:
-    print("B")
-elif score>=0.7:
-    print("C")
-elif score>=0.6:
-    print("D")
-else:
-    print("F")
+    score = input("Enter score: ")
+    score=float(score)
+
+    if score > 1.0:
+        print("Error: Out of range")
+        quit()
+    if score <0:
+        print("Error: Out of range")
+        quit()
+    elif score>=0.9:
+        print("A")
+    elif score>=0.8:
+        print("B")
+    elif score>=0.7:
+        print("C")
+    elif score>=0.6:
+        print("D")
+    else:
+        print("F")
+
+if __name__=="__main__":
+    print str(computegrade())
