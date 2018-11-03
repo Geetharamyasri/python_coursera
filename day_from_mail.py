7b086cc088c7e936c1c82ba55ae5c44e1506eae9
kszlak <kszlak@users.noreply.github.com>
2018-11-03 22:00:53 +0100

    Add functions

--- day_from_mail.py
+++ day_from_mail.py
@@ -1,25 +1,33 @@
 #program categorizes each mail by which day of the week the commit was doneself.
 #day of the week and number of appears as a key-value pair
 
-ofile = raw_input("Enter a file: ")
-try:
-    file = open(ofile)
-except:
-    exit()
+def get_dane():
+    ofile = raw_input("Enter a file: ")
+    try:
+        file = open(ofile)
+    except:
+        exit()
+    return file
 
-emails = list()
-days = list()
-dane = dict()
+def main():
+    file2 = get_dane()
+    
+    emails = list()
+    days = list()
+    dane = dict()
 
-for line in file:
-    if len(line) < 3:
-        continue
-    elif line.startswith("From") and not line.startswith("From:"):
-        emails = line.split()
-        day = emails[2]
-        days.append(day)
+    for line in file2:
+        if len(line) < 3:
+            continue
+        elif line.startswith("From") and not line.startswith("From:"):
+            emails = line.split()
+            day = emails[2]
+            days.append(day)
 
-for s in days:
-    dane[s] = dane.get(s, 0)+1
+    for s in days:
+        dane[s] = dane.get(s, 0)+1
 
-print dane
+    print dane
+
+if __name__ == "__main__":
+    main()
