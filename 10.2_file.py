7ba2a1dc8f6aa4c8d4b6df3f17a881ae5d84358b
kszlak <kszlak@users.noreply.github.com>
2018-11-10 21:11:49 +0100

    Add functions

--- 10.2_file.py
+++ 10.2_file.py
@@ -2,31 +2,38 @@
 #You can pull the hour out from the 'From ' line by finding the time and then splitting the string a second time using a colon.
 #Once you have accumulated the counts for each hour, print out the counts, sorted by hour as shown below.
 
-name = raw_input("Enter file:")
-if len(name) < 1 : name = "mbox-short.txt"
-handle = open(name)
+def data_input():
+    name = raw_input("Enter file:")
+    if len(name) < 1 : name = "mbox-short.txt"
+    handle = open(name)
+    return handle
 
-abc = list()
-time = list()
-onlyhour = list()
-pair = dict()
-newlist = list()
+def main():
+    handle2 = data_input()
+    abc = list()
+    time = list()
+    onlyhour = list()
+    pair = dict()
+    newlist = list()
 
-for line in handle:
-    if len(line)<1: continue
-    elif line.startswith("From "):
+    for line in handle2:
+        if len(line)<1: continue
+        elif line.startswith("From "):
             abc = line.split()
             time.append(abc[5])
-for i in time:
-    onlyhour.append(i[0:2])
+    for i in time:
+        onlyhour.append(i[:2])
 
-for s in onlyhour:
-    pair[s] = pair.get(s, 0)+1
+    for s in onlyhour:
+        pair[s] = pair.get(s, 0)+1
 
-for k,v in pair.items():
-    newpair = (k,v)
-    newlist.append(newpair)
-newlist = sorted(newlist)
+    for k,v in pair.items():
+        newpair = (k,v)
+        newlist.append(newpair)
+    newlist = sorted(newlist)
 
-for w,z in newlist:
-    print w,z
+    for w,z in newlist:
+        print w,z
+
+if __name__=="__main__":
+    main()
